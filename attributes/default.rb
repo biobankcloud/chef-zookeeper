564eb9dda679d33a70fd7e10b0797669b9e1ce6e
Jim Dowling <jdowling@sics.se>
2015-12-03 13:16:49 +0100

    fixed url

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 
 default[:zookeeper][:version]     = '3.4.7'
 default[:zookeeper][:checksum]    = '2e043e04c4da82fbdb38a68e585f3317535b3842c726e0993312948afcc83870'
-default[:zookeeper][:mirror]      = 'http://snurran.sics.se/hops/zookeeper'
+default[:zookeeper][:mirror]      = 'http://snurran.sics.se/hops/'
 default[:zookeeper][:user]        = 'zookeeper'
 default[:zookeeper][:install_dir] = '/opt/zookeeper'
 default[:zookeeper][:use_java_cookbook] = true
